8e21f37835b6dbb0571d789d4d33a7aa38c44aea
Mansoor Zaman <manisureal@outlook.com>
2018-05-22 16:44:58 +0100

    Created show method for lead view

--- app/controllers/leads_controller.rb
+++ app/controllers/leads_controller.rb
@@ -3,13 +3,8 @@ class LeadsController < ApplicationController
     @leads = Lead.all
   end
 
-  def search_lead
-    @new_lead = Lead.new
-    @new_lead.name = @new_lead.parsed_mail[0]
-    @new_lead.email = @new_lead.parsed_mail[1]
-    @new_lead.message = @new_lead.parsed_mail[2]
-    @new_lead.save if @new_lead.new_record?
-    redirect_to leads_path
+  def show
+    @lead = Lead.find(params[:id])
   end
 
   def new
@@ -24,6 +19,15 @@ class LeadsController < ApplicationController
     else
       render :new
     end
+  end
+
+  def search_lead
+    @new_lead = Lead.new
+    @new_lead.name = @new_lead.parsed_mail[0]
+    @new_lead.email = @new_lead.parsed_mail[1]
+    @new_lead.message = @new_lead.parsed_mail[2]
+    @new_lead.save if @new_lead.new_record?
+    redirect_to leads_path
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   get 'search_lead', to: 'leads#search_lead'
-  resources :leads, only: [:index, :new, :create]
+  resources :leads, only: [:index, :show, :new, :create]
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
