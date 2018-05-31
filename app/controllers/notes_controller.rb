3195f80ecb995e353234e33aabc417b9ba8d9611
Mansoor Zaman <manisureal@outlook.com>
2018-05-31 09:59:41 +0100

    Notes Model and Controller and routes have been added to the project

---
+++ app/controllers/notes_controller.rb
@@ -0,0 +1,10 @@
+class NotesController < ApplicationController
+  def index
+  end
+
+  def new
+  end
+
+  def create
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_05_30_113144) do
+ActiveRecord::Schema.define(version: 2018_05_31_083522) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -25,6 +25,12 @@ ActiveRecord::Schema.define(version: 2018_05_30_113144) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.boolean "viewed", default: false
+  end
+
+  create_table "notes", force: :cascade do |t|
+    t.text "content"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
