292729ceedcd348f7236429d38018c570ea0fbf6
Mansoor Zaman <manisureal@outlook.com>
2018-06-06 12:13:24 +0100

    Products has been migrated to Contacts and bootstrap select added

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_06_05_082811) do
+ActiveRecord::Schema.define(version: 2018_06_06_094300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,7 @@ ActiveRecord::Schema.define(version: 2018_06_05_082811) do
     t.bigint "lead_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "product"
     t.index ["lead_id"], name: "index_contacts_on_lead_id"
   end
 
